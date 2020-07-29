9810a2a22198bb829817b4738d25c53990e4de97
Garroti <gabryelmacedo0@gmail.com>
2020-07-28 22:10:33 -0300

    iniciando e montando estrutura basica

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
+let store = {
+  state:{
+    lista: []
+  },
+  getters:{
+    getLista: state => {
+      return state.lista
+    }
+  },
+  mutations:{
+    setLista(state, n){
+      state.lista.push(n)
+    }
+  }
+}
+
 new Vue({
   render: h => h(App),
+  store: new Vuex.Store(store),
+  router,
 }).$mount('#app')
